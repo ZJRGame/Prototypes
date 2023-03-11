e98bddfee9d14752c7b94def0fb66cfadb75e877
YDidal <1241909973@qq.com>
2023-03-11 21:17:33 +0800

    添加单例工具类

---
+++ Assets/Scripts/Utilities/MonoSingleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 继承MonoBehaviour的单例类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public bool global = true;
+    static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance =(T)FindObjectOfType<T>();
+            }
+            return instance;
+        }
+
+    }
+
+    //该类会占用Awake，继承该类时要使用Awake的话，需重写OnAwake
+    void Awake()
+    {
+        Debug.LogWarningFormat("{0}[{1}]Awake",typeof(T),this.GetInstanceID());
+        if (global)
+        {
+            if (instance != null && instance != this.gameObject.GetComponent<T>())
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            DontDestroyOnLoad(this.gameObject);
+            instance = this.gameObject.GetComponent<T>();
+        }
+        this.OnAwake();
+    }
+
+    protected virtual void OnAwake()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 普通单例类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Singleton<T> where T : new()
+{
+    static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+            return instance;
+        }
+        
+    }
+}
